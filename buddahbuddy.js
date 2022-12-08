e37c6ead6f01e03eaab17fd034dd6d4bae4549b0
JB <jacobbunnellca@gmail.com>
2022-12-08 09:45:47 -0500

    edits to Javascript file

--- buddahbuddy.js
+++ buddahbuddy.js
@@ -1,2 +1,6 @@
-var hello = alert('"What we are reluctant to touch often seems the very fabric of our salvation" - White Noise By Don Delillo');
+//var hello = alert('"What we are reluctant to touch often seems the very fabric of our salvation" - White Noise By Don Delillo');
+
+var elemDiv = document.createElement('div');
+elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
+document.body.appendChild(elemDiv);
 

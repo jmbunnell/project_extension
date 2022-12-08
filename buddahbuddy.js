e7f3764b35828c60e5761b5c1d01ae79912ed794
JB <jacobbunnellca@gmail.com>
2022-12-08 11:12:00 -0500

    Attempt to link css into javascript file

--- buddahbuddy.js
+++ buddahbuddy.js
@@ -13,7 +13,7 @@ button.setAttribute("class", "button");
                  
 // creating text to be
 //displayed on button
-var text = document.createTextNode("Button");
+var text = document.createTextNode("Click Me!");
 
                  
 // appending text to button
@@ -21,3 +21,20 @@ button.appendChild(text);
                  
 // appending button to div
 document.body.append(button);
+
+// on click produce text with quote
+
+// Get HTML head element
+        var head = document.getElementsByTagName('HEAD')[0];
+ // Create new link Element
+        var link = document.createElement('link');
+ 
+        // set the attributes for link element
+        link.rel = 'stylesheet';
+     
+        link.type = 'text/css';
+     
+        link.href = 'styles.css';
+ 
+        // Append link element to HTML head
+        head.appendChild(link);

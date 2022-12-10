45a84c4f1f8d2e201d9ed698b7ac7d86e98b371a
JB <jacobbunnellca@gmail.com>
2022-12-10 10:34:30 -0500

    not sure what im saving

--- buddahbuddy.js
+++ buddahbuddy.js
@@ -1,15 +1,11 @@
 //var hello = alert('"What we are reluctant to touch often seems the very fabric of our salvation" - White Noise By Don Delillo');
 
-//var elemDiv = document.createElement('div');
-//elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
-//document.body.appendChild(elemDiv);
-
 g = document.createElement('div');
 g.setAttribute("id", "Div1");
 
 // creating button element
 var button = document.createElement('BUTTON');
-button.setAttribute("class", "button");
+button.setAttribute("id", "button");
                  
 // creating text to be
 //displayed on button
@@ -25,16 +21,20 @@ document.body.append(button);
 // on click produce text with quote
 
 // Get HTML head element
-        var head = document.getElementsByTagName('HEAD')[0];
+var head = document.getElementsByTagName('HEAD')[0];
+
  // Create new link Element
-        var link = document.createElement('link');
+var link = document.createElement('link');
  
+
         // set the attributes for link element
         link.rel = 'stylesheet';
      
         link.type = 'text/css';
      
-        link.href = 'styles.css';
+        link.href = './styles.css';
  
         // Append link element to HTML head
         head.appendChild(link);
+
+

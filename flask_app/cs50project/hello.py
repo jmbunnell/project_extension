486d0c267cf98fe67afabe28a893d41a425ffcaa
JB <jacobbunnellca@gmail.com>
2022-12-28 15:03:20 -0500

    comments added to python code

--- flask_app/cs50project/hello.py
+++ flask_app/cs50project/hello.py
@@ -6,8 +6,11 @@ from datetime import date
 from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
 
-
+# Homepage of Application
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("layout.html")
+
+
+# Send user to new page with quote + picture?
